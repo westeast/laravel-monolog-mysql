47cd81e5cd361ecb0e50258413973f3e35ede57c
westeast <510578774@qq.com>
2021-05-25 16:47:29 +0800

    fix bugs

--- src/Logger/Monolog/Handler/MysqlHandler.php
+++ src/Logger/Monolog/Handler/MysqlHandler.php
@@ -46,10 +46,12 @@ class MysqlHandler extends AbstractProcessingHandler
         if(isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']=='on'){
             $current_url='https://';
         }
-        if($_SERVER['SERVER_PORT']!='80'){
+        if(isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT']!='80'){
             $current_url.=$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].$_SERVER['REQUEST_URI'];
         }else{
-            $current_url.=$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+            if(isset($_SERVER['SERVER_NAME'])){
+                $current_url.=$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+            }
         }
         return $current_url;
     }
